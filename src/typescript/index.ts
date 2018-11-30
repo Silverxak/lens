1df5de0ebf82cc2f2db301fc3b33e370952ea2ff
silver <silverxak@gmail.com>
2018-12-01 01:25:41 +0300

    move Pipette & Slit

--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -10,3 +10,50 @@ console.log(1);
 //         element.style.height = `${maxHeight}px`;
 //     });
 // }
+
+const prismArea: HTMLElement = document.querySelector('.prism__area');
+const prismPipette: HTMLElement = document.querySelector('.prism__pipette');
+const prismBar: HTMLElement = document.querySelector('.prism__bar');
+const prismSlit: HTMLElement = document.querySelector('.prism__slit');
+const areaLeft: number = prismArea.getBoundingClientRect().left;
+const areaTop: number = prismArea.getBoundingClientRect().top;
+const barTop: number = prismBar.getBoundingClientRect().top;
+console.log(`left: ${areaLeft} top: ${areaTop}`);
+
+prismArea.addEventListener('mousedown', e => {
+    prismPipette.style.transform = `translateX(${e.pageX - areaLeft}px) translateY(${e.pageY - areaTop}px)`;
+    console.log(Math.min(e.pageX - areaLeft, prismArea.clientWidth));
+
+    const move = e => {
+        if (Math.min(e.pageX - areaLeft, prismArea.clientWidth) > 0 && Math.min(e.pageX - areaLeft, prismArea.clientWidth) < 256 &&
+            Math.min(e.pageY - areaTop, prismArea.clientHeight) > 0 && Math.min(e.pageY - areaTop, prismArea.clientHeight) < 256) {
+            prismPipette.style.transform = `translateX(${e.pageX - areaLeft}px) translateY(${e.pageY - areaTop}px)`;
+        }
+
+    };
+    const up = e => {
+        document.removeEventListener('mousemove', move, false);
+    };
+
+
+    document.addEventListener('mousemove', move, false);
+    document.addEventListener('mouseup', up, false);
+}, false);
+
+prismBar.addEventListener('mousedown', e => {
+    prismSlit.style.transform = `translateY(${e.pageY - barTop}px)`;
+    const move = e => {
+        console.log(`${e.pageY - barTop}`);
+        if(Math.min(e.pageY - barTop, prismBar.clientHeight) > 0 && Math.min(e.pageY - barTop, prismBar.clientHeight) < 256) {
+            prismSlit.style.transform = `translateY(${e.pageY - barTop}px)`;
+        }
+    };
+    const up = e => {
+        document.removeEventListener('mousemove', move, false);
+    };
+
+
+    document.addEventListener('mousemove', move, false);
+    document.addEventListener('mouseup', up, false);
+}, false)
+
