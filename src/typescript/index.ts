8bbee943a5d40b000eb06bc3ca7c8bf69a9d3322
silver <silverxak@gmail.com>
2018-12-02 01:16:34 +0300

    small fix

--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -15,22 +15,21 @@ const prismArea: HTMLElement = document.querySelector('.prism__area');
 const prismPipette: HTMLElement = document.querySelector('.prism__pipette');
 const prismBar: HTMLElement = document.querySelector('.prism__bar');
 const prismSlit: HTMLElement = document.querySelector('.prism__slit');
-const areaLeft: number = prismArea.getBoundingClientRect().left;
-const areaTop: number = prismArea.getBoundingClientRect().top;
+
+const areaGetBoundingClientRect = prismArea.getBoundingClientRect();
+const areaLeft: number = areaGetBoundingClientRect.left;
+const areaTop: number = areaGetBoundingClientRect.top;
 const barTop: number = prismBar.getBoundingClientRect().top;
-console.log(`left: ${areaLeft} top: ${areaTop}`);
 
 prismArea.addEventListener('mousedown', e => {
     prismPipette.style.transform = `translateX(${e.pageX - areaLeft}px) translateY(${e.pageY - areaTop}px)`;
-    console.log(Math.min(e.pageX - areaLeft, prismArea.clientWidth));
 
     const move = e => {
         prismPipette.style.transform = `translateX(${Math.max(0, Math.min(e.pageX - areaLeft, prismArea.clientWidth))}px) translateY(${Math.max(0, Math.min(e.pageY - areaTop, prismArea.clientHeight))}px)`;
-
-        console.log(e.pageX - areaLeft);
     };
     const up = e => {
         document.removeEventListener('mousemove', move, false);
+        document.removeEventListener('mousemove', up, false);
     };
 
 
@@ -45,10 +44,11 @@ prismBar.addEventListener('mousedown', e => {
     };
     const up = e => {
         document.removeEventListener('mousemove', move, false);
+        document.removeEventListener('mousemove', up, false);
     };
 
 
     document.addEventListener('mousemove', move, false);
     document.addEventListener('mouseup', up, false);
-}, false)
+}, false);
 
